5da56bd7b09b82ee19db41d1b2564007622919bb
erulli <117382591+onion-milkshake@users.noreply.github.com>
2023-08-28 00:54:47 +0530

    Minor Changes

--- Frontend/dashboard/student/student_script.js
+++ Frontend/dashboard/student/student_script.js
@@ -1,15 +1,26 @@
-// document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
 
-//     // Handling of Sticky Header
-//     const header = document.getElementById("myHeader");
-//     console.log(header);
-//     const sticky = document.querySelector('.sticky');
+    // Handling of Sticky Header
+    const header = document.getElementById("myHeader");
+    const sticky = header.offsetTop;
 
-//     window.onscroll = () => {
-//         if (window.scrollY > sticky) {
-//             header.classList.add("sticky");
-//         } else {
-//             header.classList.remove("sticky");
-//         }
-//     };
-// });
+    window.onscroll = () => {
+        if (window.scrollY > sticky) {
+            header.classList.add("sticky");
+        } else {
+            header.classList.remove("sticky");
+        }
+    };
+
+    // Handling of Sticky Sidebar
+    const sidebar = document.getElementById("mySidebar");
+    const sidebarTop = sidebar.offsetTop;
+
+    window.onscroll = () => {
+        if (window.scrollY > sidebarTop) {
+            sidebar.classList.add("sticky-sidebar-active");
+        } else {
+            sidebar.classList.remove("sticky-sidebar-active");
+        }
+    };
+});
