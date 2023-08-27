da257b8afefb786da9a9f40be8ae8eecec0fc537
Mohith-Chilakala <1rn21cs044.mohith@gmail.com>
2023-08-27 15:46:07 +0530

    backend folder

---
+++ Backend/student-grievance/src/main/java/cse/rnsit/studentgrievance/StudentGrievanceApplication.java
@@ -0,0 +1,13 @@
+package cse.rnsit.studentgrievance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentGrievanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentGrievanceApplication.class, args);
+	}
+
+}
---
+++ Backend/student-grievance/src/test/java/cse/rnsit/studentgrievance/StudentGrievanceApplicationTests.java
@@ -0,0 +1,13 @@
+package cse.rnsit.studentgrievance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentGrievanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
